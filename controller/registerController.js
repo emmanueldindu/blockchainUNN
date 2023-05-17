b0092114739f6cddd91415c8ac45ab0b0119ee89
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-05-17 21:37:09 +0100

    New schema

--- controller/registerController.js
+++ controller/registerController.js
@@ -55,98 +55,123 @@ const getAlluserView = (req, res, next) => {
 }
 
 const addUser = async (req, res, next) => {
-    const data = req.body;
+  const data = req.body;
   
-    delete data.register;
+  delete data.register;
   
-    // Validate user object using Joi
-    const { error } = validateUser(data);
-    if (error) {
-      return res.status(400).json({ error: error.details[0].message });
-    }
+  // Validate user object using Joi
+  const { error } = validateUser(data);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
   
-    // Create a new user instance
-    const newUser = new User({
-      firstname: data.firstname,
-      lastname: data.lastname,
-      email: data.email,
-      phone: data.phone,
-      career: data.career,
-      state: data.state,
+  // Create a new user instance
+  const newUser = new User({
+
+
+    fullname: data.fullname, // Updated to 'fullname'
+    email: data.email,
+    phone: data.phone,
+    gender: data.gender, // Update with the appropriate property name from your data object
+    career: data.career,
+    student: data.student, // Update with the appropriate property name from your data object
+    residence: data.residence,
+    attend: data.attend // Update with the appropriate property name from your data object
+    
+    
+  });
+  
+  // Check if user with the same email or phone already exists
+  try {
+    const existingUser = await User.findOne({
+      $or: [{ email: newUser.email }, { phone: newUser.phone }],
     });
   
-    // Check if user with the same email or phone already exists
-    try {
-      const existingUser = await User.findOne({
-        $or: [{ email: newUser.email }, { phone: newUser.phone }],
-      });
-  
-      if (existingUser) {
-        return res.status(400).json({ error: 'User already registered' });
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      return res.status(500).json({ error: 'Internal server error' });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already registered' });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ error: 'Internal server error' });
   }
-  
   try {
-    const to = req.body.email
-    const name = req.body.firstname
-  
+    const to = req.body.email;
+    const name = req.body.fullname;
   
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
-  
+      host: 'smtp.hostinger.com',
+      port: 587,
+      secure: false, // Start with a plaintext connection
       auth: {
-        user: 'blockchainunnorg@gmail.com',
-        pass: 'gblbrdxbaihkqnsw',
+        user: 'admin@blockchainUNN.org',
+        pass: 'BlockchainUNN&2022',
+      },
+      tls: {
+        ciphers: 'TLSv1.2'
       }
-      
-    })
+    });
   
-    const mail_config = {
-      from: 'blockchainunnorg@gmail.com',
-      to: to,
-      subject: `Hello ${name}!!!!`,
-      text: 'thank you for registering'
-    }
+    // Upgrade the connection to SSL/TLS using STARTTLS
+    transporter
+      .verify()
+      .then(() => {
+        transporter.options.secure = true; // Set secure mode to true
+        transporter.options.tls.rejectUnauthorized = false; // Skip certificate verification (if needed)
+        return transporter.verify(); // Verify the connection again
+      })
+      .then(() => {
+        console.log('SMTP connection established securely');
+        // You can now send emails securely
+        const mailConfig = {
+          from: 'admin@blockchainUNN.org',
+          to: to,
+          subject: `Hello ${name}!!!!`,
+          text: 'Thank you for registering'
+        };
   
-    transporter.sendMail(mail_config, function (error, info) {
-      if (error) {
-        console.log(error)
-      } else {
-        console.log('mail sent')
-      }
-    })
-    
+        transporter.sendMail(mailConfig, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Mail sent');
+          }
+        });
+      })
+      .catch((error) => {
+        console.error('SMTP connection error:', error);
+      });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
+  
+  
  
 
     try {
       const savedUser = await newUser.save();
       
 
-      return  res.render('success');
-      } catch (error) {
-        console.error('Error:', error);
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-  };
+      return res.render('success');
+    } catch (error) {
+      console.error('Error:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  ;
   
 
 
   
   function validateUser(data) {
     const schema = Joi.object({
-      firstname: Joi.string().required(),
-      lastname: Joi.string().required(),
+      fullname: Joi.string().required(),
       email: Joi.string().email().required(),
-      phone: Joi.number().required(),
+      phone: Joi.string().required(),
+      gender: Joi.string().valid('Male', 'Female').required(),
       career: Joi.string().required(),
-      state: Joi.string().required(),
+      student: Joi.string().valid('Yes', 'No').required(),
+      residence: Joi.string().valid('UNN/Nsukka', 'UNEC/Enugu', 'Others').required(),
+      attend: Joi.string().valid('Yes', 'No').required()
+ 
     });
   
     return schema.validate(data);
--- models/user.js
+++ models/user.js
@@ -2,34 +2,50 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  firstname: {
+  fullname: {
     type: String,
-  },
-  lastname: {
-    type: String,
+    required: true
   },
   email: {
     type: String,
+    required: true
   },
   phone: {
-    type: Number,
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female']
   },
   career: {
     type: String,
+    enum: ['Coding/Development', 'Content Writing', 'Design', 'Community Management', 'Others']
   },
-  state: {
+  student: {
     type: String,
+    enum: ['Yes', 'No']
   },
+  residence: {
+    type: String,
+    enum: ['UNN/Nsukka', 'UNEC/Enugu', 'Others']
+  },
+  attend: {
+    type: String,
+    enum: ['Yes', 'No']
+  }
 });
 
 function validateUser(data) {
   const schema = Joi.object({
-    firstname: Joi.string().required(),
-    lastname: Joi.string().required(),
-    email: Joi.string().email().required(),
-    phone: Joi.number().required(),
-    career: Joi.string().required(),
-    state: Joi.string().required(),
+  fullname: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+  gender: Joi.string().valid('Male', 'Female').required(),
+  career: Joi.string().required(),
+  student: Joi.string().valid('Yes', 'No').required(),
+  residence: Joi.string().valid('UNN/Nsukka', 'UNEC/Enugu', 'Others').required(),
+  attend: Joi.string().valid('Yes', 'No').required()
   });
 
   return schema.validate(data);
